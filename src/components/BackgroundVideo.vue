ac9cc186e769b95c61d27a76ade1eabcbc34c0ab
wwx <1913483156@qq.com>
2024-11-10 14:09:39 +0800

    [feat] 添加背景视频组件

---
+++ src/components/BackgroundVideo.vue
@@ -0,0 +1,23 @@
+<template>
+    <video autoplay muted loop class="background-video">
+        <source :src="props.src" type="video/mp4">
+        您的浏览器不支持视频标签。
+    </video>
+</template>
+<script lang="ts" setup>
+const props = defineProps<{
+    src: string
+}>() 
+</script>
+<style scoped>
+.background-video {
+    /* position: fixed; */
+    position: absolute;
+    top: 0;
+    left: 0;
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>home</div>
+    <div>
+        <BackgroundVideo :src="backgroundVideoSrc"></BackgroundVideo> 
+    </div>
 </template>
 <script lang="ts" setup>
-
+import BackgroundVideo from "@/components/BackgroundVideo.vue";
+import { computed } from "vue";
+const backgroundVideoSrc = computed(() => {
+    return ("/src/assets/bg.mp4")
+})
 </script>
